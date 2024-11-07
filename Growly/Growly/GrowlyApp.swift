d4a29d70eae0bbc0c232ce03bb475fe9b9a187c8
Stanislaw <stanislaw.ios.dev@gmail.com>
2024-11-07 18:14:09 +0300

    project init

---
+++ Growly/Growly/GrowlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  GrowlyApp.swift
+//  Growly
+//
+//  Created by Стас Асташенко on 07/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct GrowlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
